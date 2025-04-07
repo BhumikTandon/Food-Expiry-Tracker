998f5172aae00bcf520de53776ae1e17d6b08632
Isha2004-coder <aroraishanshika@gmail.com>
2025-04-07 14:12:14 -0700

    Update Food_Expiry_TrackerApp.swift

--- Food Expiry Tracker/Food_Expiry_TrackerApp.swift
+++ Food Expiry Tracker/Food_Expiry_TrackerApp.swift
@@ -2,7 +2,7 @@
 //  Food_Expiry_TrackerApp.swift
 //  Food Expiry Tracker
 //
-//  Created by Bhumik Tandon on 3/30/25.
+//  Created by Bhumik Tandon, Ishanshika Arora and Anula Dinesh on 3/30/25.
 //
 
 import SwiftUI
